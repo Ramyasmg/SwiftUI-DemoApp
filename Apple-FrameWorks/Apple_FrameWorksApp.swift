113160994d7baeac6b1c95c92c7f1cbb09ed746b
Ramya K <ramyakrishna04smg@gmail.com>
2024-02-15 17:46:13 +0530

    Initial Commit

---
+++ Apple-FrameWorks/Apple_FrameWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameWorksApp.swift
+//  Apple-FrameWorks
+//
+//  Created by Ramya K on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
